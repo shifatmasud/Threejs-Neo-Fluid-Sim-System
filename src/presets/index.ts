adbce9a0bb07aaede7b52796769fab1d823283ca
shifatmasud <127550479+shifatmasud@users.noreply.github.com>
2025-08-31 07:55:40 +0600

    feat: Add Luminous Gel Pro preset
    Introduces a new preset, "Luminous Gel Pro," to the interactive fluid simulation. This preset offers a distinct visual style and includes a comprehensive set of configurable parameters for a richer user experience. The preset has also been added to the list of available versions.

--- src/presets/index.ts
+++ src/presets/index.ts
@@ -1,5 +1,3 @@
-
-
 import { pristineWater } from './pristineWater';
 import { liquidBubble } from './azureDepths';
 import { neoWater } from './neoWater';
@@ -7,32 +5,16 @@ import { mercury } from './mercury';
 import { crystalClear } from './crystalClear';
 import { sora } from './sora';
 import { rainbowFlare } from './rainbowFlare';
-import { livingInk } from './livingInk';
-import { luminousGel } from './luminousGel';
-import { liquidChrome } from './liquidChrome';
-import { thermal } from './thermal';
-import { deepOcean } from './deepOcean';
-import { moltenMetal } from './moltenMetal';
-import { etherealGlow } from './etherealGlow';
-import { lusion } from './lusion';
-import { awwwards } from './awwwards';
+import { luminousGelPro } from './luminousGelPro';
 import { Version } from '../types';
 
 export const versions: Version[] = [
-    liquidBubble,
-    pristineWater,
-    neoWater,
-    mercury,
-    crystalClear,
-    sora,
     rainbowFlare,
-    livingInk,
-    luminousGel,
-    liquidChrome,
-    thermal,
-    deepOcean,
-    moltenMetal,
-    etherealGlow,
-    lusion,
-    awwwards
+    luminousGelPro,
+    liquidBubble,
+    neoWater,
+    crystalClear,
+    pristineWater,
+    mercury,
+    sora
 ];
